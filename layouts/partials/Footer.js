8d72617f86d0a7f0dcc001ada2c0981228648dc0
Rasyid Kusnady <54275103+rasyidk@users.noreply.github.com>
2024-05-30 21:03:21 +0700

    config color and fonts

---
+++ layouts/partials/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <p className=" font-sans">This Is Footer</p>
+      <p className=" font-sans">This Is Footer</p>
+      <p className=" font-sans">This Is Footer</p>
+    </div>
+  );
+}
